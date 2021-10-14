19c5f5c257407557bf496db10213c0e1714048e0
KamilKsiazek93 <kamil.ksiazek.pp@gmail.com>
2021-10-14 13:20:23 +0200

    Setup project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BlockchainCreate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
