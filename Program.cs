5537a6fe47e6aac68efb84694a3d22da86a92793
KamilKsiazek93 <kamil.ksiazek.pp@gmail.com>
2021-10-19 12:09:06 +0200

    Implemented proof of work

--- Program.cs
+++ Program.cs
@@ -7,17 +7,22 @@ namespace BlockchainCreate
     {
         static void Main(string[] args)
         {
+            var startTime = DateTime.Now;
             Blockchain dummyCoin = new Blockchain();
             dummyCoin.AddBlock(new Block(DateTime.Now, null, "from:Kamil,to:Tom,amount:100"));
             dummyCoin.AddBlock(new Block(DateTime.Now, null, "from:Tom,to:Kamil,amount:30"));
             dummyCoin.AddBlock(new Block(DateTime.Now, null, "from:Kamil,to:Tom,amount:50"));
 
+            var endTime = DateTime.Now;
+            Console.WriteLine($"Duration: {endTime - startTime}");
             Console.WriteLine(JsonConvert.SerializeObject(dummyCoin, Formatting.Indented));
+
+           /* Console.WriteLine(JsonConvert.SerializeObject(dummyCoin, Formatting.Indented));
 
             Console.WriteLine($"Is Chain Valid: {dummyCoin.IsValid()}");
             Console.WriteLine($"Upate sender first transaction");
             dummyCoin.Chain[1].Data = "{from:Dominik,to:Tom,amount:100}";
-            Console.WriteLine($"Is Chain Valid: {dummyCoin.IsValid()}");
+            Console.WriteLine($"Is Chain Valid: {dummyCoin.IsValid()}");*/
         }
     }
 }
